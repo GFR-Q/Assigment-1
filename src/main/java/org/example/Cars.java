779b0662b276250f59a5feb64a0a054238c73cf5
GFR-Q <dikie1920@gmail.com>
2025-02-11 22:44:43 +0500

    second commit. adding object and adding a dependency to work with context

---
+++ src/main/java/org/example/Cars.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Cars {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
